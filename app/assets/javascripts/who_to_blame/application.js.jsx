281c39f9ed2931d6698c91c016d3df86298ecccf
Holman Gao <holman@golmansax.com>
2014-07-10 15:59:49 -0700

    Adding jshint

--- app/assets/javascripts/who_to_blame/application.js.jsx
+++ app/assets/javascripts/who_to_blame/application.js.jsx
@@ -5,82 +5,95 @@
 //= require chartjs
 //= require underscore
 
-var StatManager = React.createClass({
-  getInitialState: function () {
-    return { stats: {} };
-  },
-  componentDidMount: function () {
-    reqwest({
-      url: 'who-to-blame/footprints',
-      method: 'get',
-      success: this.handleStatsLoad
-    });
-  },
-  handleStatsLoad: function (stats) {
-    this.setState({ stats: stats });
+(function () {
+  'use strict';
 
-    var colors = [
-      {
-        color: "#F7464A",
-        highlight: "#FF5A5E"
-      },
-      {
-        color: "#46BFBD",
-        highlight: "#5AD3D1"
-      },
-      {
-        color: "#FDB45C",
-        highlight: "#FFC870"
-      }
-    ];
-
-    var colorIndex = 0;
-    var chartData = _.map(stats['rb'], function (numLines, author) {
-      console.log(author, colorIndex);
-      var colorOptions = colors[colorIndex];
-      colorIndex = (colorIndex + 1) % colors.length;
-
-      return _.extend({
-        value: numLines,
-        label: author,
-      }, colorOptions);
-    });
-
-    var ctx = window.document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx).Doughnut(chartData);
-  },
-  render: function () {
-    return (
-      <div>
-        <LoadButton onDataLoad={this.handleStatsLoad} />
-        <div>{JSON.stringify(this.state.stats)}</div>
-        <canvas id="myChart" width="400" height="400"></canvas>
-      </div>
-    );
-  }
-});
-
-var LoadButton = React.createClass({
-  loadData: function () {
-    var result = window.confirm('Are you sure you want to load data?');
-    if (result) {
+  var StatManager;
+  StatManager = React.createClass({
+    getInitialState: function () {
+      return { stats: {} };
+    },
+    componentDidMount: function () {
       reqwest({
         url: 'who-to-blame/footprints',
-        method: 'post',
-        success: function (data) {
-          this.props.onDataLoad(data);
-        }.bind(this)
+        method: 'get',
+        success: this.handleStatsLoad
       });
-    }
-  },
-  render: function () {
-    return (
-      <button onClick={this.loadData}>Load Data</button>
-    );
-  }
-});
+    },
+    handleStatsLoad: function (stats) {
+      this.setState({ stats: stats });
 
-React.renderComponent(
-  <StatManager />,
-  document.getElementById('example')
-);
+      var colors = [
+        {
+          color: '#F7464A',
+          highlight: '#FF5A5E'
+        },
+        {
+          color: '#46BFBD',
+          highlight: '#5AD3D1'
+        },
+        {
+          color: '#FDB45C',
+          highlight: '#FFC870'
+        }
+      ];
+
+      var colorIndex = 0;
+      var chartData = _.map(stats.rb, function (numLines, author) {
+        var colorOptions = colors[colorIndex];
+        colorIndex = (colorIndex + 1) % colors.length;
+
+        return _.extend({
+          value: numLines,
+          label: author
+        }, colorOptions);
+      });
+
+      var ctx = window.document.getElementById('myChart').getContext('2d');
+      new Chart(ctx).Doughnut(chartData);
+    },
+    render: function () {
+      return (
+        /* jshint ignore: start */
+        <div>
+          <LoadButton onDataLoad={this.handleStatsLoad} />
+          <div>{JSON.stringify(this.state.stats)}</div>
+          <canvas id="myChart" width="400" height="400"></canvas>
+        </div>
+        /* jshint ignore: end */
+      );
+    }
+  });
+
+  var LoadButton;
+  LoadButton = React.createClass({
+    loadData: function () {
+      var result = window.confirm('Are you sure you want to load data?');
+      if (result) {
+        reqwest({
+          url: 'who-to-blame/footprints',
+          method: 'post',
+          success: function (data) {
+            this.props.onDataLoad(data);
+          }.bind(this)
+        });
+      }
+    },
+    render: function () {
+      return (
+        /* jshint ignore: start */
+        <button onClick={this.loadData}>Load Data</button>
+        /* jshint ignore: end */
+      );
+    }
+  });
+
+  React.renderComponent(
+    (
+      /* jshint ignore: start */
+      <StatManager />
+      /* jshint ignore: end */
+    ),
+    window.document.getElementById('example')
+  );
+})();
