ae26f2337fe1d1995a2a0453d1ece5fce0e8e364
Holman Gao <holman@golmansax.com>
2014-08-02 18:05:25 -0700

    Fixing specs

--- spec/controllers/who_to_blame/snapshots_controller_spec.rb
+++ spec/controllers/who_to_blame/snapshots_controller_spec.rb
@@ -5,7 +5,7 @@ module WhoToBlame
     routes { WhoToBlame::Engine.routes }
 
     let(:params) { { format: :json } }
-    let!(:snapshot) { create(:snapshot) }
+    let!(:footprint) { create(:footprint) }
 
     describe 'routing' do
       it 'creates route for snapshots at a date' do
@@ -27,7 +27,7 @@ module WhoToBlame
       end
 
       it 'renders a json of the snapshot of today' do
-        create(:snapshot, date: Date.today)
+        create(:footprint, date: Date.today)
         get(:index, params)
 
         expected_footprint = {
@@ -35,20 +35,22 @@ module WhoToBlame
           file_type: 'rb',
           num_lines: 20,
         }.with_indifferent_access
-        snapshot =
-        expect(JSON.parse(response.body)).to eq([expected_snapshot])
+
+        snapshot = JSON.parse(response.body)
+        expect(snapshot['footprints']).to eq([expected_footprint])
       end
 
       it 'gets associated stats with a date when params specified' do
-        create(:snapshot, num_lines: 42, date: Date.new(2014, 05, 30))
+        create(:footprint, num_lines: 42, date: Date.new(2014, 05, 30))
         get(:index, params.merge(year: '2014', day: '30', month: '05'))
 
-        expected_snapshot = {
+        expected_footprint = {
           author: 'Scooby Doo',
           file_type: 'rb',
           num_lines: 42,
         }.with_indifferent_access
-        expect(JSON.parse(response.body)).to eq([expected_snapshot])
+        snapshot = JSON.parse(response.body)
+        expect(snapshot['footprints']).to eq([expected_footprint])
       end
     end
 
@@ -69,40 +71,32 @@ module WhoToBlame
         end.to raise_error(ActionController::UnknownFormat)
       end
 
-      it 'renders a json of the basic snapshots and loads stats into db' do
-        expected_output = [
-          {
-            year: 2014,
-            month: 7,
-            day: 4,
-            snapshots: [holmans_ruby_snapshot(408).as_json],
-          }.with_indifferent_access,
-          {
-            year: 2014,
-            month: 7,
-            day: 6,
-            snapshots: [holmans_ruby_snapshot(567).as_json],
-          }.with_indifferent_access,
-          {
-            year: 2014,
-            month: 7,
-            day: 8,
-            snapshots: [holmans_ruby_snapshot(812).as_json],
-          }.with_indifferent_access,
-        ]
+      it 'renders a json of latest snapshot and loads stats into db' do
+        expected_snapshot = {
+          year: 2014,
+          month: 7,
+          day: 8,
+          footprints: [holmans_ruby_footprint(812).as_json],
+        }.with_indifferent_access
 
         post(:create, create_params)
         rendered_json = JSON.parse(response.body)
-        expect(rendered_json).to eq(expected_output)
+        expect(rendered_json).to eq(expected_snapshot)
 
         expect(Author.find_by_full_name('Holman Gao')).not_to be_nil
         expect(FileType.all.map(&:name)).to eq(['rb'])
-        expect(snapshot.count).to be > 0
+
+        start_date = Date.new(2014, 7, 4)
+        expected_dates = [start_date, start_date + 2, start_date + 4]
+        expect(Footprint.all.map(&:date)).to eq(expected_dates)
+
+        expected_lines = [408, 567, 812]
+        expect(Footprint.all.map(&:num_lines)).to eq(expected_lines)
       end
     end
 
-    def holmans_ruby_snapshot(num_lines)
-      Basicsnapshot.new('Holman Gao', 'rb', num_lines)
+    def holmans_ruby_footprint(num_lines)
+      BasicFootprint.new('Holman Gao', 'rb', num_lines)
     end
   end
 end
